201286f471617b9b6cd169622f9f512b8c42429e
Dan Kadera <me@subsonic.cz>
2021-01-29 23:40:26 +0100

    Step 3: Introduce ComponentEvents

--- src/Components/CommentCount.php
+++ src/Components/CommentCount.php
@@ -5,13 +5,25 @@ declare(strict_types=1);
 namespace App\Components;
 
 use App\Model\BlogModel;
+use App\Model\Events\CommentCreatedEvent;
+use App\Model\Events\CommentDeletedEvent;
 use Nette\Application\UI\Control;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class CommentCount extends Control
+class CommentCount extends Control implements EventSubscriberInterface
 {
   private BlogModel $model;
   private int $postId;
   private ?int $comments = null;
+
+  public static function getSubscribedEvents() : array
+  {
+    return [
+      CommentCreatedEvent::class => 'refresh',
+      CommentDeletedEvent::class => 'refresh',
+    ];
+  }
+
 
   public function __construct(BlogModel $model, int $postId)
   {
@@ -33,5 +45,10 @@ class CommentCount extends Control
     $this->template->count = $this->getCommentCount();
     $this->template->setFile(__DIR__ . '/templates/commentCount.latte');
     $this->template->render();
+  }
+
+  public function refresh() : void
+  {
+    $this->redrawControl();
   }
 }
